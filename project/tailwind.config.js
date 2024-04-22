5e273d8b831e442ff13e7ee528a7da4aef09eca9
KasAgg21 <73023696+KasAgg21@users.noreply.github.com>
2024-04-22 22:21:54 +0530

    Add files via upload

---
+++ project/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,jsx}"],
+  theme: {
+    extend: {
+      colors:{
+        'or':'#FA7921',
+        'ge':'#566E3D',
+        'bl':'#0D0A0B',
+        'lge':'#647F46',
+        'cre':'#FFF9EC'
+      }
+    },
+  },
+  plugins: [],
+}
+
